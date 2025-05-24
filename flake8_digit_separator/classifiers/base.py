57078552f516b817f088494d5ae074b50dc9b730
Timur Valiev <cptchunk@yandex.ru>
2025-05-24 23:50:22 +0500

    token type and selftype for classifier

--- flake8_digit_separator/classifiers/base.py
+++ flake8_digit_separator/classifiers/base.py
@@ -1,25 +1,30 @@
 from abc import ABC, abstractmethod
+from typing import TypeAlias, TypeVar
 
 from flake8_digit_separator.fds_numbers.base import Number
+from flake8_digit_separator.types import TokenLikeStr
+
+SelfClassifier = TypeVar('SelfClassifier', bound='Classifier')
+LowerTokenLikeStr: TypeAlias = str
 
 
 class Classifier(ABC):
     @property
     @abstractmethod
-    def token(self) -> str:
+    def token(self: SelfClassifier) -> TokenLikeStr:
         ...
 
     @property
     @abstractmethod
-    def token_lower(self):
+    def token_lower(self: SelfClassifier) -> LowerTokenLikeStr:
         ...
 
     @abstractmethod
-    def classify(self) -> Number:
+    def classify(self: SelfClassifier) -> Number:
         ...
 
 
 class BaseClassifier(Classifier):
     @property
-    def token_lower(self) -> str:
+    def token_lower(self) -> LowerTokenLikeStr:
         return self.token.lower()
