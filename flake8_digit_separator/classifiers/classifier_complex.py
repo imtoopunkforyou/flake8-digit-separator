79264956dcea22221ad0a6b63c818e2b158e3547
Timur Valiev <cptchunk@yandex.ru>
2025-05-24 23:54:36 +0500

    typevar for selfcomplexclassifier

--- flake8_digit_separator/classifiers/classifier_complex.py
+++ flake8_digit_separator/classifiers/classifier_complex.py
@@ -1,15 +1,20 @@
+from typing import TypeVar
+
 from flake8_digit_separator.classifiers.base import BaseClassifier
 from flake8_digit_separator.fds_numbers.fds_numbers import ComplexNumber
+from flake8_digit_separator.types import TokenLikeStr
+
+SelfComplexClassifier = TypeVar('SelfComplexClassifier', bound='ComplexClassifier')
 
 
 class ComplexClassifier(BaseClassifier):
-    def __init__(self, token: str) -> None:
+    def __init__(self: SelfComplexClassifier, token: TokenLikeStr) -> None:
         self._token = token
 
-    def classify(self) -> ComplexNumber | None:
+    def classify(self: SelfComplexClassifier) -> ComplexNumber | None:
         if 'j' in self.token_lower:
             return ComplexNumber(self.token_lower)
 
     @property
-    def token(self) -> str:
+    def token(self: SelfComplexClassifier) -> str:
         return self._token
