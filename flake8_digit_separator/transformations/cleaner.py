18628632ba34ade5cb32b00d05010a699f575125
Timur Valiev <cptchunk@yandex.ru>
2025-05-25 20:16:05 +0500

    docs: docstr for cleaner

--- flake8_digit_separator/transformations/cleaner.py
+++ flake8_digit_separator/transformations/cleaner.py
@@ -6,14 +6,19 @@ SelfCleaner = TypeVar('SelfCleaner', bound='Cleaner')
 
 @final
 class Cleaner:
+    """Clears the text by `pattern` argument."""
+
     pattern: ClassVar[re.Pattern[str]] = re.compile(r'[+\-_]')
+    """`+`, `-` and `_` symbols."""
 
     def __init__(self: SelfCleaner, text: str) -> None:
         self._text = text
 
     @property
     def text(self: SelfCleaner) -> str:
+        """Uncleaned text."""
         return self._text
 
     def clean(self: SelfCleaner) -> str:
+        """Cleaned text."""
         return self.pattern.sub('', self._text)
